75497b97204395d2a123d174a6edb42d12a3bf33
preparatoriovirtualcomdelta <deltaigualb.bmenos4.a.c@gmail.com>
2026-03-29 15:45:34 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBPx6l8JDNFfi70-DgRBvWk4mhuQz2kOWQ",
   authDomain: "preparatoriovirtualcomdelta.firebaseapp.com",
   projectId: "preparatoriovirtualcomdelta",
-  storageBucket: "preparatoriovirtualcomdelta.appspot.com",
+  storageBucket: "preparatoriovirtualcomdelta.firebasestorage.app",
   messagingSenderId: "554101533814",
   appId: "1:554101533814:web:ed46583d92f4e14679c357"
 };
